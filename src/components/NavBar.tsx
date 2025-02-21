22cf20c4dd68c4fd36d1a039940f7e07a327566a
Jean Marc Mufind <makayamufind@gmail.com>
2025-02-21 07:58:20 -0500

    update NavBar component to include ColorModeButton and adjust layout

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,11 @@
-import { HStack, Image, Text } from "@chakra-ui/react";
+import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/Logo/logo.webp";
+import { ColorModeButton } from "./ui/color-mode";
 const NavBar = () => {
   return (
-    <HStack>
+    <HStack justifyContent='space-between' padding="10px">
       <Image src={logo} alt="logo" boxSize={"60px"} />
-      <Text>NavBar</Text>
+      <ColorModeButton />
     </HStack>
   );
 };
